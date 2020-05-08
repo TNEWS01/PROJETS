7726db5626d9b1b1fdd73969e225809ab168869b
TN <thierry.news@free.fr>
2020-05-08 12:18:12 +0200

    Premiere modif sur branche test3

--- prj_01/prj_01_script_05.py
+++ prj_01/prj_01_script_05.py
@@ -1,3 +1,8 @@
 # Script TEST : prj_01_script_05.py
+<<<<<<< HEAD
 # Premiere modif sur branche master
 # Deuxieme modif sur branche master
+=======
+
+# Premiere modif sur branche test3
+>>>>>>> Premiere modif sur branche test3
