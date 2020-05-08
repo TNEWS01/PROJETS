b49eafa58077f3063391ed93adc6ea7b254f1949
TN <thierry.news@free.fr>
2020-05-08 12:36:34 +0200

    Correction de prj_01_script_05.py

--- prj_01/prj_01_script_05.py
+++ prj_01/prj_01_script_05.py
@@ -1,12 +1,6 @@
 # Script TEST : prj_01_script_05.py
-<<<<<<< HEAD
 # Premiere modif sur branche master
 # Deuxieme modif sur branche master
-=======
-
 # Premiere modif sur branche test3
-<<<<<<< HEAD
->>>>>>> Premiere modif sur branche test3
-=======
 # Deusieme modif sur branche test3
 # Troisème modif sur branche test3
