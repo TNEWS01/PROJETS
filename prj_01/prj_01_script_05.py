1bcd5bcde0ef55ac5f061cb52f2a0e245b2a9dd4
TN <thierry.news@free.fr>
2020-05-08 12:13:54 +0200

    Modif de prj_01_script_05.py

--- prj_01/prj_01_script_05.py
+++ prj_01/prj_01_script_05.py
@@ -1,11 +1,1 @@
 # Script TEST : prj_01_script_05.py
-
-# Deuxième modif sur branche master
-
-# Troisième modif sur branche master 
-
-# Premiere modif sur branche test1
-
-# Premiere modif sur la branche test1
-
-
